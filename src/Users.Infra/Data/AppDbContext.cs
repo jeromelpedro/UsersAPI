a4a06decdfcb9f76f69514375c86d5ca820d67ae
Tony Abrunhosa <ti.tony.abrunhosa@lg.com.br>
2025-11-27 01:54:28 -0300

    AJuste migration

--- src/Users.Infra/Data/AppDbContext.cs
+++ src/Users.Infra/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Users.Domain.Entity;
 
 namespace Users.Infra.Data
@@ -14,6 +15,26 @@ namespace Users.Infra.Data
 			base.OnModelCreating(modelBuilder);
 
 			modelBuilder.Entity<Usuario>();				
+		}
+	}
+
+	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+	{
+		public AppDbContext CreateDbContext(string[] args)
+		{
+			// Aqui vamos pegar a connection string de variável de ambiente,
+			// que é simples, seguro e comum no mercado.
+
+			var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+			if (string.IsNullOrWhiteSpace(connectionString))
+				throw new InvalidOperationException(
+					"Variável de ambiente 'ConnectionStrings__DefaultConnection' não definida.");
+
+			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+			optionsBuilder.UseSqlServer(connectionString);
+
+			return new AppDbContext(optionsBuilder.Options);
 		}
 	}
 }
---
+++ src/Users.Infra/Migrations/20251127042846_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Users.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    DataCriacaoUsuario = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataAlteracaoSenha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
