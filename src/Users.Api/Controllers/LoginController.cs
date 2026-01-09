882799c41659e62c5eda7dc61780a6bc5d4d9f84
Tony Abrunhosa <ti.tony.abrunhosa@lg.com.br>
2026-01-09 14:49:54 -0300

    ajustes login

--- src/Users.Api/Controllers/LoginController.cs
+++ src/Users.Api/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Users.Application.Interfaces;
+using Users.Domain.Dto;
 
 namespace Users.Api.Controllers
 {
@@ -8,9 +9,9 @@ namespace Users.Api.Controllers
 	public class LoginController(IJwtService service) : ControllerBase
 	{
 		[HttpPost]
-		public async Task<IActionResult> Login(string Email, string Senha)
+		public async Task<IActionResult> Login([FromQuery] LoginDto login)
 		{
-			var token = await service.Autenticar(Email, Senha);
+			var token = await service.Autenticar(login.Email, login.Senha);
 
 			return token is not null
 				? Ok(token)
---
+++ src/Users.Domain/Dto/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Users.Domain.Dto
+{
+	public class LoginDto
+	{
+		public string Email { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Senha { get; set; }
+	}
+}
