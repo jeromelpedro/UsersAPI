bee3e00443af77802fd804d8f3d1493059c0e443
Tony Abrunhosa <ti.tony.abrunhosa@lg.com.br>
2026-03-18 10:20:42 -0300

    AJuste no log traces

--- src/Users.Api/Program.cs
+++ src/Users.Api/Program.cs
@@ -1,5 +1,6 @@
-using Serilog;
+using Microsoft.ApplicationInsights.Extensibility;
 using OpenTelemetry.Trace;
+using Serilog;
 using Users.Api.Configurations;
 using Users.Api.Middlewares;
 using Users.Application;
@@ -7,29 +8,31 @@ using Users.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationInsightsTelemetry();
+
+// pega do container
+var telemetryConfiguration = builder.Services.BuildServiceProvider()
+	.GetRequiredService<TelemetryConfiguration>();
+
 Log.Logger = new LoggerConfiguration()
 	.MinimumLevel.Information()
 	.Enrich.FromLogContext()
 	.Enrich.With(new Users.Api.Serilog.ActivityEnricher())
 	.WriteTo.Console()
+	.WriteTo.ApplicationInsights(
+		telemetryConfiguration,
+		TelemetryConverter.Traces)
 	.CreateLogger();
 
 builder.Logging.ClearProviders();
-builder.Logging.AddSerilog(Log.Logger, dispose: true);
+builder.Logging.AddSerilog(Log.Logger);
+
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerConfiguration();
 builder.Services.ResolveDependencyInjection(builder.Configuration);
 builder.Services.AddAuthConfiguration(builder.Configuration);
-
-// Application Insights (reads CONNECTION STRING from APPLICATIONINSIGHTS_CONNECTION_STRING env var)
-builder.Services.AddApplicationInsightsTelemetry();
-
-// Note: Application Insights SDK is registered above and will collect requests/dependencies.
-// If you want full OpenTelemetry instrumentation with OTLP exporters, we can enable it later.
-
-// Http context accessor required by Audit interceptor
 builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
